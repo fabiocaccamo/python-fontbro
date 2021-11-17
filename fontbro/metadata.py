d48b8df7642042c885a701c57966375dea493d81
Fabio Caccamo <fabio.caccamo@gmail.com>
2021-11-17 23:54:51 +0100

    Updated metadata description and classifiers. [ci skip]

--- fontbro/metadata.py
+++ fontbro/metadata.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Fabio Caccamo'
 __copyright__ = 'Copyright (c) 2021-present Fabio Caccamo'
-__description__ = 'human-friendly font operations.'
+__description__ = 'friendly font operations.'
 __email__ = 'fabio.caccamo@gmail.com'
 __license__ = 'MIT'
 __title__ = 'python-fontbro'
