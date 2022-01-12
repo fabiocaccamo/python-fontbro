0b72647c8135f6ab6ccc40c1e31a6a2be3498b89
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-01-12 22:51:28 +0100

    Added test script for generating unicode data.

---
+++ tests/test_update_unicode_data.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from fontbro import Font
+from fontTools import unicodedata
+
+from tests import AbstractTestCase
+
+import fsutil
+import json
+
+
+class UpdateUnicodeDataTestCase(AbstractTestCase):
+    """
+    Test case for updating the pre-computed unicode data.
+    """
+
+    def test_update_unicode_data(self):
+        blocks = []
+        blocks_cache = {}
+        scripts = []
+        scripts_cache = {}
+        for code in range(0, 0x110000):
+            block_name = unicodedata.block(code)
+            if block_name == "No_Block":
+                continue
+            block = {
+                "name": block_name,
+            }
+            Font._populate_unicode_items_set(blocks, blocks_cache, block)
+            script_tag = unicodedata.script(code)
+            script_name = unicodedata.script_name(script_tag)
+            script = {
+                "name": script_name,
+                "tag": script_tag,
+            }
+            Font._populate_unicode_items_set(scripts, scripts_cache, script)
+
+        for block in blocks:
+            block["characters_total"] = block["characters_count"]
+            block.pop("characters_count", None)
+
+        for script in scripts:
+            script["characters_total"] = script["characters_count"]
+            script.pop("characters_count", None)
+
+        fsutil.write_file(
+            fsutil.join_path(__file__, "../data/blocks.json"),
+            json.dumps(blocks, sort_keys=True, indent=4),
+        )
+        fsutil.write_file(
+            fsutil.join_path(__file__, "../data/scripts.json"),
+            json.dumps(scripts, sort_keys=True, indent=4),
+        )
