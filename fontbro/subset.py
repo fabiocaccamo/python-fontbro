60f773ed0e7785a3fcd6df0e7a45d467f1b9f7ba
Fabio Caccamo <fabio.caccamo@gmail.com>
2024-03-07 01:01:41 +0100

    Add type hints to `flags`, `math`, `subset` and `utils` modules.

--- fontbro/subset.py
+++ fontbro/subset.py
@@ -1,22 +1,30 @@
+from __future__ import annotations
+
 import re
+
+from collections.abc import Iterable
 
 from fontTools.subset import parse_unicodes as _parse_unicodes
 
 
-def parse_unicodes(unicodes):
-    unicodes = unicodes or ""
+def parse_unicodes(
+    unicodes: Iterable[int | str] | str,
+) -> list[int]:
     if isinstance(unicodes, (list, set, tuple)):
         # convert possible int codepoints to hex str
-        unicodes = list(unicodes)
-        for index in range(len(unicodes)):
-            code = unicodes[index]
-            unicodes[index] = f"{code:04X}" if isinstance(code, int) else code
-        unicodes = ",".join(list(set(unicodes)))
-    assert isinstance(unicodes, str)
+        unicodes_list = list(unicodes)
+        for index in range(len(unicodes_list)):
+            code = unicodes_list[index]
+            unicodes_list[index] = f"{code:04X}" if isinstance(code, int) else code
+        unicodes_str = ",".join(list(set(unicodes_list)))
+    elif isinstance(unicodes, str):
+        unicodes_str = unicodes
+    else:
+        raise ValueError("Invalid 'unicodes' value.")
+    assert isinstance(unicodes_str, str)
     # replace possible — ‐ − (&mdash; &dash; &minus;) with -
-    unicodes = re.sub(r"[\—\‐\−]", "-", unicodes)
+    unicodes_str = re.sub(r"[\—\‐\−]", "-", unicodes_str)
     # remove U+, \u, u if present
-    unicodes = re.sub(r"(U\+)|(\\u)|(u)", "", unicodes, flags=re.I)
-    unicodes = _parse_unicodes(unicodes)
-    # print(unicodes)
-    return unicodes
+    unicodes_str = re.sub(r"(U\+)|(\\u)|(u)", "", unicodes_str, flags=re.I)
+    unicodes_list = _parse_unicodes(unicodes_str)
+    return unicodes_list
