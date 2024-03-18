620be43ddfbe45f45767bd9a4e26246e471801ab
Fabio Caccamo <fabio.caccamo@gmail.com>
2024-03-18 21:42:50 +0100

    Bump `mypy`.

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mypy fontbro --install-types --non-interactive
+mypy fontbro --install-types --non-interactive --strict
 coverage erase
 coverage run --append --show-missing unittest discover
 coverage report -m
