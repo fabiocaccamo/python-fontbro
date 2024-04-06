0fec9619c453ba4a20ca8f67126a0943ca0ab769
Fabio Caccamo <fabio.caccamo@gmail.com>
2024-04-06 17:56:09 +0200

    Update `mypy` options.

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mypy fontbro --follow-imports="skip" --ignore-missing-imports --install-types --non-interactive --strict
+mypy fontbro --ignore-missing-imports --install-types --non-interactive --strict
 coverage erase
 coverage run --append --source=fontbro -m unittest
 coverage report -m --show-missing --ignore-errors
